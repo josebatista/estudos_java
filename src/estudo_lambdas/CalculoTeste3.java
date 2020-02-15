1a0efbc75eab84153f262613473f3b343c265b0a
jpereira <josebatistapereira@gmail.com>
2020-02-15 11:16:54 -0300

    Utilizando interface funcional implementada pelo java para realizar os calculos

---
+++ src/estudo_lambdas/CalculoTeste3.java
@@ -0,0 +1,32 @@
+package estudo_lambdas;
+
+import java.util.function.BinaryOperator;
+
+/**
+ * Realizando os calculos utilizando interface funcional implementada pelo java.
+ * 
+ * @author jpereira
+ *
+ */
+public class CalculoTeste3 {
+
+	public static void main(String[] args) {
+
+		BinaryOperator<Double> calc = (x, y) -> {
+			return x + y;
+		};
+		System.out.println(calc.apply(2.0, 3.0));
+		
+		calc = (x, y) -> x * y;
+		System.out.println(calc.apply(2.0, 3.0));
+
+		BinaryOperator<Integer> calc2 = (x, y) -> {
+			return x + y;
+		};
+		System.out.println(calc2.apply(2, 3));
+		
+		calc2 = (x, y) -> x * y;
+		System.out.println(calc2.apply(2, 3));
+
+	}
+}
