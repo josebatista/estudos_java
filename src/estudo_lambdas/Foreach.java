379516c4c1f288efc269e4ae3914dad241209fd3
jpereira <josebatistapereira@gmail.com>
2020-02-15 11:35:11 -0300

    Personalizando funcao para impressao ao ser utilizando na Lambda e referencia

--- src/estudo_lambdas/Foreach.java
+++ src/estudo_lambdas/Foreach.java
@@ -17,9 +17,19 @@ public class Foreach {
 		System.out.println("\nLambda #01...");
 		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
 
-		System.out.println("\nMethod Reference...");
+		System.out.println("\nMethod Reference #01...");
 		aprovados.forEach(System.out::println);
 
+		System.out.println("\nLambda #02...");
+		aprovados.forEach(nome -> minhaImpressao(nome));
+
+		System.out.println("\nMethod Reference #02...");
+		aprovados.forEach(Foreach::minhaImpressao);
+
+	}
+
+	static void minhaImpressao(String nome) {
+		System.out.println("Ola " + nome + " tudo certo?");
 	}
 
 }
