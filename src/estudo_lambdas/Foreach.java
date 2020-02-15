bd64412500868fc4ea7a47a6527eb213fbdd1821
jpereira <josebatistapereira@gmail.com>
2020-02-15 11:26:56 -0300

    Apresentando itens de uma lista com foreach

---
+++ src/estudo_lambdas/Foreach.java
@@ -0,0 +1,25 @@
+package estudo_lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+
+		List<String> aprovados = Arrays.asList("Jose", "Isaac", "Natali");
+
+		System.out.println("Forma tradicional...");
+		for (String nome : aprovados) {
+			System.out.println(nome);
+		}
+
+		System.out.println("\nLambda #01...");
+		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
+
+		System.out.println("\nMethod Reference...");
+		aprovados.forEach(System.out::println);
+
+	}
+
+}
