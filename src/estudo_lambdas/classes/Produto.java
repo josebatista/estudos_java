a96aeba13e542b530219111c46dd75d13aac219c
jpereira <josebatistapereira@gmail.com>
2020-02-15 16:57:00 -0300

    criado classe produto para validar a interface funcional Predicate

---
+++ src/estudo_lambdas/classes/Produto.java
@@ -0,0 +1,39 @@
+package estudo_lambdas.classes;
+
+public class Produto {
+
+	private String nome;
+	private double preco;
+	private double desconto;
+
+	public Produto(String nome, double preco, double desconto) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
+	}
+
+}
