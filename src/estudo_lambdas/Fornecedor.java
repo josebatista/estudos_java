d595cb88f80bab218e02da1151658530ad95bd4b
jpereira <josebatistapereira@gmail.com>
2020-02-16 21:56:11 -0300

    Utilizando a interface funcional Supplier

---
+++ src/estudo_lambdas/Fornecedor.java
@@ -0,0 +1,17 @@
+package estudo_lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Fornecedor {
+
+	public static void main(String[] args) {
+
+		Supplier<List<String>> umaLista = () -> Arrays.asList("Jose", "Isaac", "Natali");
+
+		System.out.println(umaLista.get());
+
+	}
+
+}
