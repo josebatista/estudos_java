cb85d167ad766595e3e61f134d73680016896c72
jpereira <josebatistapereira@gmail.com>
2020-02-19 20:05:11 -0300

    Adicionando atributos iniciais na classe Campo

--- src/jogo_campo_minado/modelo/Campo.java
+++ src/jogo_campo_minado/modelo/Campo.java
@@ -1,5 +1,22 @@
 package jogo_campo_minado.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+
+	private final int linha;
+	private final int coluna;
+
+	private boolean aberto = false;
+	private boolean minado = false;
+	private boolean marcado = false;
+
+	private List<Campo> vizinhos = new ArrayList<>();
+
+	Campo(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
 
 }
