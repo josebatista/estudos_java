1dcaaef73840fcf9c6301e0c1b7301c0923a0712
jpereira <josebatistapereira@gmail.com>
2020-02-19 20:38:25 -0300

    implementado funcao para adicionar campo como vizinho. implementado alguns testes para validar o funcionamento da funcao

--- src/jogo_campo_minado/modelo/Campo.java
+++ src/jogo_campo_minado/modelo/Campo.java
@@ -19,4 +19,24 @@ public class Campo {
 		this.coluna = coluna;
 	}
 
+	boolean adicionarVizinho(Campo vizinho) {
+		boolean linhaDiferente = this.linha != vizinho.linha;
+		boolean colunaDiferente = this.coluna != vizinho.coluna;
+		boolean diagonal = linhaDiferente && colunaDiferente;
+
+		int deltaLinha = Math.abs(this.linha - vizinho.linha);
+		int deltaColuna = Math.abs(this.coluna - vizinho.coluna);
+		int deltaGeral = deltaColuna + deltaLinha;
+
+		if (deltaGeral == 1 && !diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		} else if (deltaGeral == 2 && diagonal) {
+			vizinhos.add(vizinho);
+			return true;
+		}
+
+		return false;
+	}
+
 }
---
+++ test/jogo_campo_minado/modelo/CampoTeste.java
@@ -0,0 +1,61 @@
+package jogo_campo_minado.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jogo_campo_minado.modelo.Campo;
+
+class CampoTeste {
+
+	private Campo campo;
+
+	@BeforeEach
+	void setup() {
+		campo = new Campo(3, 3);
+	}
+
+	@Test
+	void testeVizinhoDistancia1Direita() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoDistancia1Esquerda() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoDistancia1EmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoDistancia1EmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeVizinhoDistancia2() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+
+}
