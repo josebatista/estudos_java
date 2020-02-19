61a19bbadee2e9009c156841aa384587c923913d
jpereira <josebatistapereira@gmail.com>
2020-02-19 20:51:03 -0300

    Implementado funcao para abrir juntamente com os vizinhos seguros, e criado uma excecao para tratar no caso de uma explosao

--- src/jogo_campo_minado/modelo/Campo.java
+++ src/jogo_campo_minado/modelo/Campo.java
@@ -3,6 +3,8 @@ package jogo_campo_minado.modelo;
 import java.util.ArrayList;
 import java.util.List;
 
+import jogo_campo_minado.excecao.ExplosaoException;
+
 public class Campo {
 
 	private final int linha;
@@ -29,14 +31,40 @@ public class Campo {
 		int deltaGeral = deltaColuna + deltaLinha;
 
 		if (deltaGeral == 1 && !diagonal) {
-			vizinhos.add(vizinho);
+			this.vizinhos.add(vizinho);
 			return true;
 		} else if (deltaGeral == 2 && diagonal) {
-			vizinhos.add(vizinho);
+			this.vizinhos.add(vizinho);
 			return true;
 		}
 
 		return false;
+	}
+
+	void alternarMarcacao() {
+		if (!this.aberto) {
+			this.marcado = !this.marcado;
+		}
+	}
+
+	boolean abrir() {
+		if (!this.aberto && !this.marcado) {
+			if (this.minado) {
+				throw new ExplosaoException();
+			}
+
+			if (this.vizinhancaSegura()) {
+				this.vizinhos.forEach(v -> v.abrir());
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	boolean vizinhancaSegura() {
+		return this.vizinhos.stream().noneMatch(v -> v.minado);
 	}
 
 }
