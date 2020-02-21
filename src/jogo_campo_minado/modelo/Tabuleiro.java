d5167d27c6c8b878ab03c73ebd24a37215324143
jpereira <josebatistapereira@gmail.com>
2020-02-20 22:29:02 -0300

    Implementados funcoes iniciais

--- src/jogo_campo_minado/modelo/Tabuleiro.java
+++ src/jogo_campo_minado/modelo/Tabuleiro.java
@@ -1,5 +1,45 @@
 package jogo_campo_minado.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
+
+	private int linhas;
+	private int colunas;
+	private int minas;
+
+	private final List<Campo> campos = new ArrayList<Campo>();
+
+	public Tabuleiro(int linhas, int colunas, int minas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		this.minas = minas;
+
+		gerarCampos();
+		associarVizinhos();
+		sortearMinas();
+	}
+
+	private void gerarCampos() {
+		for (int linha = 0; linha < this.linhas; linha++) {
+			for (int coluna = 0; coluna < this.colunas; coluna++) {
+				campos.add(new Campo(linha, coluna));
+			}
+		}
+	}
+
+	private void associarVizinhos() {
+		for (Campo c1 : campos) {
+			for (Campo c2 : campos) {
+				c1.adicionarVizinho(c2);
+			}
+		}
+	}
+
+	private void sortearMinas() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
