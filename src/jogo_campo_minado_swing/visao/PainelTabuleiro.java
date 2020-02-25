f8dfd3e61eecf7061cc7d180f404480d6b9dd459
Jose Batista Pereira <jpereira>
2020-02-24 22:38:57 -0300

    Adicionando interface grafica ao jogo, criando a tela principal e o painel do tabuleiro onde ficarao outros componentes graficos como os botoes

---
+++ src/jogo_campo_minado_swing/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package jogo_campo_minado_swing.visao;
+
+import javax.swing.JPanel;
+
+import jogo_campo_minado_swing.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+	}
+
+}
