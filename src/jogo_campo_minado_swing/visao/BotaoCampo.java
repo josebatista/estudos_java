15ebdc556db9eadf2cca8367bbf209fac21165a9
Jose Batista Pereira <jpereira>
2020-02-24 23:05:52 -0300

    implementando visual dos botoes do campo

--- src/jogo_campo_minado_swing/visao/BotaoCampo.java
+++ src/jogo_campo_minado_swing/visao/BotaoCampo.java
@@ -1,15 +1,67 @@
 package jogo_campo_minado_swing.visao;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 import jogo_campo_minado_swing.modelo.Campo;
+import jogo_campo_minado_swing.modelo.CampoEvento;
+import jogo_campo_minado_swing.modelo.CampoObservador;
 
 @SuppressWarnings("serial")
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador {
+
+	private static final Color BG_PADRAO = new Color(184, 184, 184);
+	private static final Color BG_MARCAR = new Color(8, 179, 247);
+	private static final Color BG_EXPLODIR = new Color(189, 66, 68);
+	private static final Color TEXTO_VERDE = new Color(0, 100, 0);
+
+	private Campo campo;
 
 	public BotaoCampo(Campo campo) {
-		
-		
-		
+		this.campo = campo;
+
+		setBackground(BG_PADRAO);
+		setBorder(BorderFactory.createBevelBorder(0));
+
+		this.campo.registrarObservador(this);
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		switch (evento) {
+		case ABRIR:
+			aplicarEstiloAbrir();
+			break;
+		case MARCAR:
+			aplicarEstiloMarcar();
+			break;
+		case EXPLODIR:
+			aplicarEstiloExplodir();
+			break;
+		default:
+			aplicarEstiloPadrao();
+		}
+	}
+
+	private void aplicarEstiloAbrir() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloMarcar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloExplodir() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void aplicarEstiloPadrao() {
+		// TODO Auto-generated method stub
+
 	}
 }
