f99e6844c98d726c19a36eff6edcecf216d21fa1
Jose Batista Pereira <jpereira>
2020-02-29 21:31:09 -0300

    Implementacao inicial do componente display

--- src/swing/calculadora/visao/Display.java
+++ src/swing/calculadora/visao/Display.java
@@ -1,8 +1,26 @@
 package swing.calculadora.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
+
+	private final JLabel label;
+
+	public Display() {
+		setBackground(new Color(46, 49, 50));
+		this.label = new JLabel("1234,56");
+		this.label.setForeground(Color.WHITE);
+		this.label.setFont(new Font("courier", Font.PLAIN, 30));
+
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		add(this.label);
+
+	}
 
 }
