d0e7adb21c657de8a8c9d94b33460e37efe77a53
Jose Batista Pereira <jpereira>
2020-02-29 22:07:34 -0300

    Finalizada implementacao grafica da calculadora

--- src/swing/calculadora/visao/Teclado.java
+++ src/swing/calculadora/visao/Teclado.java
@@ -20,28 +20,41 @@ public class Teclado extends JPanel {
 
 		setLayout(layout);
 
+		// adicionando peso para ocupar todos o espaco da tela
+		c.weightx = 1;
+		c.weighty = 1;
+
+		// preenchando na duas coordenadas (x, y)
+		c.fill = GridBagConstraints.BOTH;
+
+		// Linha 1
+		c.gridwidth = 3;
 		adicionarBotao("AC", COR_CINZA_ESCURO, c, 0, 0);
-		adicionarBotao("+/-", COR_CINZA_ESCURO, c, 1, 0);
-		adicionarBotao("%", COR_CINZA_ESCURO, c, 2, 0);
+		c.gridwidth = 1;
 		adicionarBotao("/", COR_LARANJA, c, 3, 0);
 
+		// Linha 2
 		adicionarBotao("7", COR_CINZA_CLARO, c, 0, 1);
 		adicionarBotao("8", COR_CINZA_CLARO, c, 1, 1);
 		adicionarBotao("9", COR_CINZA_CLARO, c, 2, 1);
 		adicionarBotao("*", COR_LARANJA, c, 3, 1);
 
+		// Linha 3
 		adicionarBotao("4", COR_CINZA_CLARO, c, 0, 2);
 		adicionarBotao("5", COR_CINZA_CLARO, c, 1, 2);
 		adicionarBotao("6", COR_CINZA_CLARO, c, 2, 2);
 		adicionarBotao("-", COR_LARANJA, c, 3, 2);
 
+		// Linha 4
 		adicionarBotao("1", COR_CINZA_CLARO, c, 0, 3);
 		adicionarBotao("2", COR_CINZA_CLARO, c, 1, 3);
 		adicionarBotao("3", COR_CINZA_CLARO, c, 2, 3);
 		adicionarBotao("+", COR_LARANJA, c, 3, 3);
 
+		// Linha 5
+		c.gridwidth = 2;
 		adicionarBotao("0", COR_CINZA_CLARO, c, 0, 4);
-		adicionarBotao("0", COR_CINZA_CLARO, c, 1, 4);
+		c.gridwidth = 1;
 		adicionarBotao(",", COR_CINZA_CLARO, c, 2, 4);
 		adicionarBotao("=", COR_LARANJA, c, 3, 4);
 
