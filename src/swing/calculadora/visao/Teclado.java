7e560755fe55a0af65e05a9be6b524cb794a233e
Jose Batista Pereira <jpereira>
2020-02-29 21:42:00 -0300

    Implementado botao e atribuido as cores que serao utilizadas pelo teclado

--- src/swing/calculadora/visao/Teclado.java
+++ src/swing/calculadora/visao/Teclado.java
@@ -1,8 +1,18 @@
 package swing.calculadora.visao;
 
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel {
+
+	private static final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
+	private static final Color COR_CINZA_CLARO = new Color(99, 99, 99);
+	private static final Color COR_LARANJA = new Color(242, 163, 60);
+
+	public Teclado() {
+
+	}
 
 }
