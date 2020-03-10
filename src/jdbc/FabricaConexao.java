76a5027d878d05b03009005e171b564208a4c94f
Jose Batista Pereira <jpereira>
2020-03-09 21:36:01 -0300

    criado classe fabrica para agilizar na criacao da conexao com o banco

---
+++ src/jdbc/FabricaConexao.java
@@ -0,0 +1,21 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+	private static final String URL = "jdbc:mysql://localhost:3306?verifyServerCertificate=false&useSSL=true";
+	private static final String USUARIO = "root";
+	private static final String SENHA = "root";
+
+	public static Connection getConexao() {
+		try {
+			return DriverManager.getConnection(URL, USUARIO, SENHA);
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
