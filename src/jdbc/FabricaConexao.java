c88b7b34bb7091513d28b8bca29e76a2fd9c9a8b
Jose Batista Pereira <jpereira>
2020-03-09 23:25:42 -0300

    externalizando dados de conexao

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -1,21 +1,34 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
-	private static final String URL = "jdbc:mysql://localhost:3306/curso_java?verifyServerCertificate=false&useSSL=true";
-	private static final String USUARIO = "root";
-	private static final String SENHA = "root";
-
 	public static Connection getConexao() {
 		try {
+
+			Properties prop = getProperties();
+
+			final String URL = prop.getProperty("banco.url");
+			final String USUARIO = prop.getProperty("banco.usuario");
+			final String SENHA = prop.getProperty("banco.senha");
+
 			return DriverManager.getConnection(URL, USUARIO, SENHA);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties";
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+
+		return prop;
 	}
 
 }
